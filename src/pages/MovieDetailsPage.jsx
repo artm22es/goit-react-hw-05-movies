561c04b3ab11accbaa0fc638c204b67999ec3bbb
artm22es <artm22es@gmail.com>
2024-01-09 11:47:00 +0100

    Update MovieDetailsPage.jsx

--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -1,30 +1,30 @@
-import { fetchMovieDetails } from 'api';
-import { useEffect, useRef, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+// import { fetchMovieDetails } from 'api';
+// import { useEffect, useRef, useState } from 'react';
+// import { useLocation, useParams } from 'react-router-dom';
 
 export default function MovieDetailsPage() {
-  const [movie, setMovie] = useState(null);
-  const [error, setError] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const params = useParams();
-  const location = useLocation();
-  const backLink = useRef(location.state?.from ?? '/movies');
+  // const [movie, setMovie] = useState(null);
+  // const [error, setError] = useState(false);
+  // const [isLoading, setIsLoading] = useState(false);
+  // const params = useParams();
+  // const location = useLocation();
+  // const backLink = useRef(location.state?.from ?? '/movies');
 
-  useEffect(() => {
-    async function getDetails() {
-      try {
-        setIsLoading(true);
-        setError(false);
-        const data = await fetchMovieDetails(params.movieId);
-        setMovie(data);
-      } catch (error) {
-        setError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    get();
-  }, [params.movieId]);
+  // useEffect(() => {
+  //   async function getDetails() {
+  //     try {
+  //       setIsLoading(true);
+  //       setError(false);
+  //       const data = await fetchMovieDetails(params.movieId);
+  //       setMovie(data);
+  //     } catch (error) {
+  //       setError(true);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
+  //   getDetails();
+  // }, [params.movieId]);
 
   return <div>Movie details page</div>;
 }
