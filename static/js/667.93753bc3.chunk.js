87e26aad9418dca3c20fe7f241f16f27a0d91767
artm22es <125702817+artm22es@users.noreply.github.com>
2024-01-11 16:21:38 +0000

    Deploying to gh-pages from @ artm22es/goit-react-hw-05-movies@d04dc21b2695635b840ee3d7838bd0cae2155340 🚀

---
+++ static/js/667.93753bc3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[667],{667:function(e,t,r){r.r(t),r.d(t,{default:function(){return u}});var n=r(184);function u(){return(0,n.jsx)("div",{children:"Movies page"})}}}]);
+//# sourceMappingURL=667.93753bc3.chunk.js.map
